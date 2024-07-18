c2438961575888bafdac26ca6e7782645df8987d
Morote <43614132+Grazerquart@users.noreply.github.com>
2024-07-18 15:39:56 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function getValues(){
+    let messageInput = document.getElementById("message").value;
+    let alertMessage = document.getElementById("alertMessage");
+    alertMessage.innerText = messageInput;
+}
